fc233603303037dd5b7fffa5e16245a36efbe78b
unknown <belmerabet@gmail.com>
2026-04-21 16:21:01 +0200

    home

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,7 @@
 import type { Route } from "./+types/home";
 import Navbar from "../../components/Navbar";
+import {ArrowBigRight, ArrowRight, LayersIcon, LucideClock1} from "lucide-react";
+import Button from "../../components/ui/Button";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -10,9 +12,76 @@ export function meta({}: Route.MetaArgs) {
 
 export default function Home() {
   return (
-      <div>
+      <div className="home">
         <Navbar />
-        <h1 className="text-3xl text-indigo-700 font-extrabold">Home</h1>
+        <section className="hero">
+            <div className="announce">
+                <div className="dot">
+                    <div className="pulse">
+
+                    </div>
+                </div>
+                <p>Introducing Stanzea v1.0</p>
+            </div>
+            <h1>Build beautiful spaces at the speed of thought with Stanzea</h1>
+            <p className="subtitle"> Stanzea is an AI-first design environment that helps you visualize, render, and ship architectural projects faster than ever.</p>
+            <div className="actions">
+                <a className="cta" href="#upload">
+                    Start Building <ArrowRight className="icon" />
+                </a>
+                <Button variant="outline" size="lg" className="demo">
+                    Watch Demo
+                </Button>
+            </div>
+            <div id="upload" className="upload-shell">
+                <div className="grid-overlay" />
+                <div className="upload-card">
+                    <div className="upload-head">
+                        <div className="upload-icon">
+                            <LayersIcon className="icon" />
+                        </div>
+                        <h3>Upload your floor plan</h3>
+                        <p>Supports JPG, PNG formats up to 10 MB</p>
+                    </div>
+                    <p>Upload Images</p>
+                </div>
+            </div>
+        </section>
+          <section className="projects">
+            <div className="section-inner">
+                <div className="section-head">
+                    <div className="copy">
+                        <h2>Projects</h2>
+                        <p>Your latest work and shared community projects, all in one place.</p>
+                    </div>
+                </div>
+                <div className="projects-grid">
+                    <div className="project-card group">
+                        <div className="preview">
+                            <img src="https://roomify-mlhuk267-dfwu1i.puter.site/projects/1770803585402/rendered.png" alt="project"/>
+                            <div className="badge">
+                                <span>
+                                    Community
+                                </span>
+                            </div>
+                        </div>
+                        <div className="card-body">
+                            <div>
+                                <h3>Project Milano</h3>
+                                <div className="meta">
+                                    <LucideClock1 size={12}/>
+                                    <span>{new Date("08.05.2026").toDateString()}</span>
+                                    <span>By Islam Belmerabet</span>
+                                </div>
+                            </div>
+                            <div className="arrow">
+                                <ArrowBigRight size={18}/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+          </section>
       </div>
   );
 }
